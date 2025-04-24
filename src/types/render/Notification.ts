2dd7c66b5da98812387bea62659582b142556cdb
Lunatica-Luna <devlead@gongroin.com>
2025-04-24 17:10:22 +0900

    알림 타입 변경

--- src/types/render/Notification.ts
+++ src/types/render/Notification.ts
@@ -1,6 +1,6 @@
 export interface Notification {
   id: string
-  message: { mode: 'string' | 'i18n'; ns?: string; value: string }
+  message: { mode: 'string' | 'i18n'; ns?: string; value: string; props?: Record<string, string> }
   type: 'success' | 'error' | 'info' | 'warning'
   duration?: number
   isRemoving?: boolean
