155eb3fdc294d6b0262bd6181aa2b3a7640f3a19
OwO <admin@q-owo-p.space>
2025-04-27 21:08:44 +0900

    Update src/main/modules/ocr-manager/ocr-manager.service.ts
    Co-authored-by: codacy-production[bot] <61871480+codacy-production[bot]@users.noreply.github.com>

--- src/main/modules/ocr-manager/ocr-manager.service.ts
+++ src/main/modules/ocr-manager/ocr-manager.service.ts
@@ -33,7 +33,7 @@ export interface OCRResultInfo {
 
 // OCR 추출 결과 인터페이스
 export interface ExtractedRegionsResult {
-  regions: { [key: string]: Buffer }
+  regions: Record<string, Buffer>
   texts: Record<string, string>
 }
 
